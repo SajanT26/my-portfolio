f9190e3806428a29705ee2dad484af3c4d9ca41d
Sajan <n01715615@humber.ca>
2024-11-14 00:21:05 -0500

    Made contact form and fixed up some styiling

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select all buttons with the class .show-details-btn
     const buttons = document.querySelectorAll('.show-details-btn');
-
-    // Loop over each button to add event listeners
     buttons.forEach(button => {
         button.addEventListener('click', function() {
-            // Select the project details associated with the button
             const details = this.nextElementSibling;
 
-            // Toggle visibility and button text
+          
             if (details.style.display === "none" || details.style.display === "") {
-                details.style.display = "block";      // Show details
-                this.textContent = "Hide Details";    // Change button text
+                details.style.display = "block";      
+                this.textContent = "Hide Details";    
             } else {
-                details.style.display = "none";       // Hide details
-                this.textContent = "Show Details";    // Change button text back
+                details.style.display = "none";    
+                this.textContent = "Show Details";  
             }
         });
     });
